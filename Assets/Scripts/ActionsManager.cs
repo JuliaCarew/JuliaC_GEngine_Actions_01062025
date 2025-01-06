7c866acf80f917c99aa4d1232daecd74264396e7
JuliaCarew <crewjulia3@gmail.com>
2025-01-06 15:27:35 -0400

    Initial commit

---
+++ Assets/Scripts/ActionsManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionsManager : MonoBehaviour
+{
+    public static class Actions
+    {
+        public static Action partyTimeEvent;
+    }
+
+    private void OnEnable()
+    {
+        Actions.partyTimeEvent += ReadInput;
+    }
+    private void OnDisable()
+    {
+        
+    }
+    void ReadInput()
+    {
+        if (Input.GetKeyDown("Space"))
+        {
+            //Actions.partyTimeEvent;
+            Debug.Log("Space pressed, action taken");
+        }
+    }
+}
